f45e4efc86daeeee7193593a404c3dd0049fad20
root <root@ttllkk-new.zapto.org>
2025-01-05 19:00:31 +0100

    add helpers
    Signed-off-by: root <root@ttllkk-new.zapto.org>

---
+++ test-sync.sh
@@ -0,0 +1,11 @@
+curl -X POST \
+	     -H "Content-Type: application/json" \
+	          --data '{
+       "jsonrpc": "2.0",
+              "method": "eth_syncing",
+	             "params": [],
+		            "id": 1
+			         }' \
+					      http://127.0.0.1:18545
+
+#curl -d '{"id":0,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false]}'   -H "Content-Type: application/json" http://localhost:18545
