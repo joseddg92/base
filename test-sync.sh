161d0b053a9bce6022cc8dee4dced42c1230ad4a
root <root@ttllkk-new.zapto.org>
2025-01-06 13:20:12 +0100

    fix test-sync.sh
    Signed-off-by: root <root@ttllkk-new.zapto.org>

--- test-sync.sh
+++ test-sync.sh
@@ -1,11 +1,4 @@
-curl -X POST \
-	     -H "Content-Type: application/json" \
-	          --data '{
-       "jsonrpc": "2.0",
-              "method": "eth_syncing",
-	             "params": [],
-		            "id": 1
-			         }' \
-					      http://127.0.0.1:18545
-
-curl -d '{"id":0,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false]}' -H "Content-Type: application/json" http://localhost:18545 | jq '.result.number'
+curl -X POST -H "Content-Type: application/json"  --data '{ "jsonrpc": "2.0", "method": "eth_syncing", "params": [], "id": 1}'   http://localhost:18545
+echo ""
+curl -d '{"id":0,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false]}' -H "Content-Type: application/json" http://localhost:18545 2> /dev/null | jq '.result.number' 
+echo ""
