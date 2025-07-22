3117c5117e805ce89cde8a4b8d2a88a410efa9e1
Jose Diaz <joseddg92@gmail.com>
2025-07-22 10:24:56 +0200

    update-jdepedro-branch.sh: improvements
    Signed-off-by: Jose Diaz <joseddg92@gmail.com>

--- update-jdepedro-branch.sh
+++ update-jdepedro-branch.sh
@@ -6,6 +6,7 @@ done
 TAG=$(git for-each-ref --sort=-creatordate --format '%(refname:short)' refs/tags | head -n 1)
 echo "Latest tag: $TAG"
 
-git checkout $TAG
 git checkout -b jdepedro/$TAG
-echo "Now run: git rebase jdepedro/<previous_tag>; then solve errors and commit"
+git rebase $TAG
+
+echo "Now solve errors and commit"
