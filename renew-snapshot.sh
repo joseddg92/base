1206d8397a07d6c41f15b87edb58bb589b5150f7
root <root@ttllkk-new.zapto.org>
2025-10-18 20:48:42 +0200

    [wip]
    Signed-off-by: root <root@ttllkk-new.zapto.org>

--- renew-snapshot.sh
+++ renew-snapshot.sh
@@ -1,2 +1,9 @@
 # Just clear the folder and start the node again (in snap mode)
 rm -rf geth-data/*
+rm -rf /big-box/nodes/base/
+
+
+mkdir -p geth-data/geth/chaindata
+mkdir -p /big-box/nodes/base/geth-data/geth/chaindata/ancient/
+
+ln -s /big-box/nodes/base/geth-data/geth/chaindata/ancient/ geth-data/geth/chaindata/ancient
